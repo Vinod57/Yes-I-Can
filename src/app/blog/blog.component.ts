34f54cad239297a3134307dc3308438a200b8096
Vinod <vinod112331@gmail.com>
2018-11-04 17:04:13 +0530

    css changes

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog.component.css']
 })
 export class BlogComponent implements OnInit {
-
+  myVar = true;
   constructor() { }
 
   ngOnInit() {
